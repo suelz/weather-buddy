015539703fb258897a3d41295d08ed1197fbfd50
suelz <conorpadraigkennedy@gmail.com>
2020-01-01 13:10:33 -0500

    heroku changes

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (event) =>{
 
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+    fetch('/weather?address=' + location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             m1.textContent=data.error
